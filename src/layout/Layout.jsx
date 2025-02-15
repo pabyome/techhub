25e8431cdd126950161baf1c89e42030047b18fc
Paul <abayomipaulhenryhill@gmail.com>
2025-02-15 22:45:34 +0000

    updated import

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
